3572b765c049e2ec14ae6bad06645ace1074fe85
Rohit Akurdekar <114896370+RohitAkurdekar@users.noreply.github.com>
2022-10-17 15:50:54 +0530

    Concept of class
    Class and import

---
+++ class/person_class.py
@@ -0,0 +1,48 @@
+# class example
+class Person:
+    # private variables
+    __workBehave = ""
+    __studyBehave = ""
+    # init variables
+    def __init__(self,rcv_name,rcv_Sex,rcv_Profession)->None:
+        #public variables, Init at obj creation
+        self.Name = rcv_name
+        self.Sex = rcv_Sex
+        self.Profession = rcv_Profession
+        
+    # set Name
+    def setName(self,rcv_name)->None:
+        self.Name = rcv_name
+    
+    # set Sex
+    def setSex(self,rcv_Sex)->None:
+        self.Sex = rcv_Sex
+    
+    # set profession
+    def setProfession(self,rcv_Profession)->None:
+        self.Profession = rcv_Profession
+    
+    # set working behaviour
+    def setWorkBehaviour(self,rcv_workBehave)->None:
+        self.__workBehave = rcv_workBehave
+    
+    # set study behaviour
+    def setStudyBehaviour(self)->None:
+        self.__studyBehave = input("Enter study behaviour: ")
+
+    # get working behaviour
+    def getWorkBehaviour(self)->str:
+        return self.__workBehave
+    
+    # get study behaviour
+    def getStudyBehaviour(self)->str:
+        return self.__studyBehave
+
+    #display all details
+    def displayDet(self)->None:
+        print("Name: ",self.Name)
+        print("Sex: ",self.Sex)
+        print("Profession: ",self.Profession)
+        print("Study():",self.getStudyBehaviour())
+        print("Work():",self.getWorkBehaviour())
+    
---
+++ class/person_main.py
@@ -0,0 +1,20 @@
+from person_class import Person
+
+def main():
+# Rohit is an Object of a class person
+    Rohit = Person("Rohit","Male","Engineer")
+    Rohit.setStudyBehaviour()
+    Rohit.setWorkBehaviour("Never studies")     # multiple ways to pass data
+
+    print(Rohit.displayDet())       # calling func to display details
+
+
+# Saurav is an Object of a class person
+    Saurav = Person("Saurav","Male","Engineer")
+    Saurav.setStudyBehaviour()      # setting study behaviour
+         # multiple ways to pass data
+    Saurav.setWorkBehaviour(input("Enter work behaviour: ")) 
+
+    print(Saurav.displayDet())       # calling func to display details
+
+main()
